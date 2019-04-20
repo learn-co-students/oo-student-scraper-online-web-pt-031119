b72bcd10bd8293d6a8e1fa81fd49c832c29a68ca
Ryan <kerneltux0@gmail.com>
2019-04-20 16:06:22 -0600

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,20 +7,42 @@ class Scraper
   def self.scrape_index_page(index_url)
     student_info = []
     learn_site = Nokogiri::HTML(open(index_url))
+    # binding.pry
     section = learn_site.css("div.roster-cards-container")
-    students = section.css("div.student-card")
+    #students = section.css("div.student-card")
     student_hash = {}
     
-    students.map do |student_data|
-      student_hash[:location] = student_data.css("div.card-text-container p.student-location").text
-      student_hash[:name] = student_data.css("div.card-text-container h4.student-name").text
+    section.each do |student_data|
+      student_data.css("div.card-text-container p.student-location").each do |text|
+        student_hash[:location] = text.text
+      end
+
+      student_data.css("div.card-text-container h4.student-name").each do |text|
+        student_hash[:name] = text.text
+      end
+  
       student_hash[:profile_url] = student_data.css("a").attribute("href").value
+
       student_info << student_hash
       # binding.pry
+      student_info
 
     end
 
     student_info
+
+    # <div class="student-card" id="kevin-mccormack-card">
+    #         <a href="students/kevin-mccormack.html">
+    #           <div class="view-profile-div">
+    #             <h3 class="view-profile-text">View Profile</h3>
+    #           </div>
+    #           <div class="card-text-container">
+    #             <h4 class="student-name">Kevin McCormack</h4>
+    #             <p class="student-location">New York, NY</p>
+    #           </div>
+    #         </a>
+    #       </div>
+
     
 
     # :name = student_data.css("div.card-text-container h4.student-name").text
