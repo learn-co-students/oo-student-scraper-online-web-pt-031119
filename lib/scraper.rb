600f6a645351a47738d1906ea1a1f9fa3c175a90
Alex Merecka <merecka@gmail.com>
2019-04-24 03:29:18 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,30 +20,35 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     html = open(profile_url)
     doc = Nokogiri::HTML(html)
-    student_profile = []
-    #binding.pry
+    student_profile = {}
     doc.css(".vitals-container").collect do |student|
       student.css(".social-icon-container").collect do |media|
-        binding.pry
         media.css("a").each_with_index do |url, index|
-          for index <= media.css("a").count
-            if media.css("a")[index]["href"].include? "twitter"
-              twitter_url = media.css("a")[index]["href"]
-            elsif media.css("a")[index]["href"].include? "linkedin"
-              linkedin_url = media.css("a")[index]["href"]
-            elsif media.css("a")[index]["href"].include? "github"
-              github_url = media.css("a")[index]["href"]
-            elsif media.css("a")[index]["href"].end_with?(".com/")
-              blog_url = media.css("a")[index]["href"]
-            end
+          if media.css("a")[index]["href"].include? "twitter"
+            twitter_url = media.css("a")[index]["href"]
+            student_profile[:twitter] = twitter_url
+          elsif media.css("a")[index]["href"].include? "linkedin"
+            linkedin_url = media.css("a")[index]["href"]
+            student_profile[:linkedin] = linkedin_url
+          elsif media.css("a")[index]["href"].include? "github"
+            github_url = media.css("a")[index]["href"]
+            student_profile[:github] = github_url
+          elsif media.css("a")[index]["href"].end_with?(".com/")
+            blog_url = media.css("a")[index]["href"]
+            student_profile[:blog] = blog_url
           end
         end
       end
-      # if media.css("a")[0]["href"].include? "twitter"
-      #   twitter_url = media.css("a")[0]["href"]
-      # elsif media.css("a")[0]["href"].include? ""
-
+      quote = student.css(".vitals-text-container").css(".profile-quote").text
+      student_profile[:profile_quote] = quote
     end
-  end
 
-end
+    doc.css(".details-container").collect do |student|
+      bio_quote = student.css(".description-holder").css("p").text
+      student_profile[:bio] = bio_quote
+    end
+   student_profile
+
+  end #end of scrape_profile_page method
+
+end #end Scraper class
