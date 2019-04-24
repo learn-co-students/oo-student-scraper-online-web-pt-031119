e57b946dd4a203677c503905702654ebbd912759
Alex Merecka <merecka@gmail.com>
2019-04-24 06:10:54 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -17,7 +17,6 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-  #  binding.pry
     attributes_hash.each do |student|
       self.send(:twitter=, attributes_hash[:twitter])
       self.send(:linkedin=, attributes_hash[:linkedin])
@@ -26,6 +25,7 @@ class Student
       self.send(:profile_quote=, attributes_hash[:profile_quote])
       self.send(:bio=, attributes_hash[:bio])
     end
+    self
   end
 
   def self.all
