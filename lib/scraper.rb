b02b573965a5e8f15e89569217b0eaf10374efe5
Alex Merecka <merecka@gmail.com>
2019-04-24 02:11:46 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,7 +25,24 @@ class Scraper
     doc.css(".vitals-container").collect do |student|
       student.css(".social-icon-container").collect do |media|
         binding.pry
+        media.css("a").each_with_index do |url, index|
+          for index <= media.css("a").count
+            if media.css("a")[index]["href"].include? "twitter"
+              twitter_url = media.css("a")[index]["href"]
+            elsif media.css("a")[index]["href"].include? "linkedin"
+              linkedin_url = media.css("a")[index]["href"]
+            elsif media.css("a")[index]["href"].include? "github"
+              github_url = media.css("a")[index]["href"]
+            elsif media.css("a")[index]["href"].end_with?(".com/")
+              blog_url = media.css("a")[index]["href"]
+            end
+          end
+        end
       end
+      # if media.css("a")[0]["href"].include? "twitter"
+      #   twitter_url = media.css("a")[0]["href"]
+      # elsif media.css("a")[0]["href"].include? ""
+
     end
   end
 
