033ca01c568ff463701d97f45ca3bdad99e82866
Leeya Davis <leeya463@gmail.com>
2019-04-27 17:52:28 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,27 +5,30 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    html = File.read(index_url)
-    flatiron = Nokogiri::HTML(html)
-
+    flatiron = Nokogiri::HTML(open(index_url))
     profiles = []
-#binding.pry
     flatiron.css("div.student-card").each do |profile|
       profiles << {
         name: profile.css("h4.student-name").text,
         location: profile.css("p.student-location").text,
-        profile_url: profile(".student-card").attr("href")
-        #profile_url: profile.css("div.student-card").attribute("href").value
+        profile_url: profile.css("a").attr("href").value
       }
-      #binding.pry
     end
     profiles
-    #binding.pry
-    #return an array of hashes, each hash rep a single student
-    #hash keys name, location, profile_url
   end
 
   def self.scrape_profile_page(profile_url)
+    profile_page = Nokogiri::HTML(open(profile_url))
+    binding.pry
+    { twitter_url: profile_page.css("div.social-icon-container a").attr("href").value,
+      #linkedin_url:
+      #twitter_url:
+      #linkedin_url:
+      #github_url:
+      #blog:url:
+      #profile_quote:
+      #bio:
+    }
 
   end
 
