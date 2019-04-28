8a4fde59ccf208e62a70617d4883e9dea7ab4527
Leeya Davis <leeya463@gmail.com>
2019-04-28 12:15:17 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,42 +6,27 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     flatiron = Nokogiri::HTML(open(index_url))
-    profiles = []
-    flatiron.css("div.student-card").each do |profile|
-      profiles << {
+
+    flatiron.css("div.student-card").map do |profile|
+      {
         name: profile.css("h4.student-name").text,
         location: profile.css("p.student-location").text,
         profile_url: profile.css("a").attr("href").value
       }
     end
-    profiles
   end
 
   def self.scrape_profile_page(profile_url)
     profile_page = Nokogiri::HTML(open(profile_url))
 
-    social = [:twitter, :linkedin, :github, :blog]
     links = profile_page.css("div.social-icon-container a").map {|link| link["href"]}
-
-    social_hash = social.each_index.with_object({}) do |i, h|
-      if links[i].include?(social[i].to_s)
-        h[social[i]] = links[i]
-        #binding.pry
-      else
-        h[social[3]] = links[-1]
-        #binding.pry
-      end
-      #binding.pry
-    end.delete_if {|k,v| v == nil}
-
-    profile = {
+    {
+      twitter: links.find {|e| e.include?("twitter")},
+      linkedin: links.find {|e| e.include?("linkedin")},
+      github: links.find {|e| e.include?("github")},
+      blog: links.find {|e| e.end_with?(".com/")},
       profile_quote: profile_page.css("div.profile-quote").text,
       bio: profile_page.css("div.description-holder p").text
-    }
-    final_hash = social_hash.merge(profile)
-
-    final_hash
-    #binding.pry
+    }.compact
   end
-
 end
