1670cc870f6949f8edc4ac82338c76e7af82e411
Mark Kanko <mkanko66@gmail.com>
2019-04-28 15:02:47 -0700

    completed oo-student-scraper lab

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,7 +12,7 @@ class Scraper
     #student_hash = {}
 
     doc.css("div.student-card").each do |card|
-      student_name = card.css(".student-name").text 
+      student_name = card.css(".student-name").text
       student_location = card.css(".student-location").text
       student_profile_url = card.css("a").attribute("href").value
       student_hash = {:name => student_name, :location => student_location, :profile_url => student_profile_url}
@@ -28,26 +28,56 @@ class Scraper
     
     doc = Nokogiri::HTML(open(profile_url))
     #binding.pry 
-
     student_profile = {}
 
-    #binding.pry 
-
-    doc.css("div.social-icon-container").each do |profile|
-       
-      binding.pry 
+    links = doc.css("div.social-icon-container").children.css("a").collect do |element|
+      element.attribute('href').value
     end 
-    #student_profile 
+    links.each do |link|
+      if link.include?("twitter")
+        student_profile[:twitter] = link
+      elsif link.include?("linkedin")
+        student_profile[:linkedin] = link
+      elsif link.include?("github")
+        student_profile[:github] = link 
+      else 
+        student_profile[:blog] = link 
+      end 
+    end
+    #binding.pry 
+  # doc.css("div.vitals-text-container").each do |item|
+  #   student_profile[:profile_quote] = item.css("div.profile-quote").text
+
+    student_profile[:profile_quote] = doc.css("div.profile-quote").text
+       
+  # doc.css("div.details-container").each do |detail|
+  #   student_profile[:bio] = detail.css("div.description-holder p").text.strip 
+
+    student_profile[:bio] = doc.css("div.description-holder p").text.strip 
+    student_profile 
   end
   
 end
+
+# .children.css("a")
+#
+# element.attribute('href').value
+#  if link.include?("linkedin")
+#         student[:linkedin] = link
+#  else
+#  student[:blog] = link
+
+
 
  
 
 # scrape_profile_page
 
-# doc.css("a").attribute("href").value                     collection of profiles to iterate over
-# doc.css("div.view-profile-div").first.css(".view-profile-text").text
+# doc.css("div.social-icon-container")
+# doc.css("div.social-icon-container").children.css("a")                              collection of profiles to iterate over
+# doc.css("div.social-icon-container").first.css("a").attribute("href").value         the twitter url of an individual student
+# doc.css("div.vitals-text-container").first.css("div.profile-quote").text 
+
 
 
 
