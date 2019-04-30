d6f9c3c6c89bbe4088cc282f1c98789ad8199e0b
chandra mohan <mohaniyer_78@hotmail.com>
2019-04-30 01:30:20 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,3 +13,6 @@ class Scraper
 
 end
 
+# find CSS selector for parent of all pictures.
+#iterate over each picture and extract the name, location, profile- url
+# create a hash for every picture and push it to array.
