a0fa43b0fc95ec20276a34832f247078e9e01ef0
chandra mohan <mohaniyer_78@hotmail.com>
2019-05-01 02:55:50 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,19 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    students = []
+    doc = Nokogiri::HTML(open(index_url))
+    doc.css(".roster.cards.container").each do |card|
+      binding.pry
+      card.css(".student-card a").each do |student|
+        
+         student_name = student.css('.student-name').text
+       end 
+     end
   end
 
   def self.scrape_profile_page(profile_url)
@@ -16,3 +25,5 @@ end
 # find CSS selector for parent of all pictures.
 #iterate over each picture and extract the name, location, profile- url
 # create a hash for every picture and push it to array.
+
+
